3a72e191b46a42839cf8f92ce2faee04f9b892e4
MarceloTorres06 <129771499+MarceloTorres06@users.noreply.github.com>
2026-02-04 20:47:59 -0300

    fix2

--- backend_test.py
+++ backend_test.py
@@ -5,7 +5,7 @@ import json
 from datetime import datetime
 
 class SuperMarketAPITester:
-    def __init__(self, base_url="https://super-admin.preview.emergentagent.com"):
+    def __init__(self, base_url="https://minimarket-snowy.vercel.app"):
         self.base_url = base_url
         self.api_url = f"{base_url}/api"
         self.tokens = {}
